a0503b349347d74c7d1e2b6ddbb476fa19b1293b
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-27 13:33:31 +0200

    exo start

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+        <Navigation>
+            
+        </Navigation>
+        </div>
+    );
+};
+
+export default About;
